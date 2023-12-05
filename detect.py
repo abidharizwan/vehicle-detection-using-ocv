711385fa16cd1d79ad004dc8e65eddc55c2bc8c8
abiidharizwan <abidharizwann@gmail.com>
2023-12-05 11:46:41 +0530

    Vehicle Detection And Counting Project

--- detect.py
+++ detect.py
@@ -34,13 +34,15 @@ while True:
     dilatada=cv2.morphologyEx(dilatada,cv2.MORPH_CLOSE,kernel)
     counterShape,h=cv2.findContours(dilatada, cv2. RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cv2.line (frame1, (25, count_line_position), (1200,count_line_position), (255,127,0), 3)
-
     for (i,c) in enumerate(counterShape):
         (x,y,w,h) = cv2.boundingRect(c)
         validate_counter = (w>= min_width_rect) and (h>= min_height_rect)
         if not validate_counter:
             continue
         cv2.rectangle(frame1, (x,y), (x+w, y+h),(0,255,0), 2)
+        cv2.putText(frame1, "Vehicle"+str(counter),(x, y-20), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 244, 0),3)
+
+
         center=center_handle(x,y,w,h)
         detect.append(center)
         cv2.circle(frame1,center,4, (0,0, 255), -1)
@@ -51,7 +53,7 @@ while True:
             cv2.line (frame1, (25, count_line_position), (1200,count_line_position), (0,127,255), 3)
             detect.remove((x,y))
             print(f"Vehicle counter {str(counter)}")
-    cv2.putText(frame1,"VEHICLE COUNTER:"+str(counter),(450,70),cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255),5)
+    cv2.putText(frame1,"VEHICLE COUNTER:"+str(counter),(450,70),cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0),5)
     cv2.imshow('Video Original',frame1)
     if cv2.waitKey(1) ==13:
         break
